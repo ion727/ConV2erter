8e32a87f3fa0930fff03a76979b18469db595111
ion727 <151961936+ion727@users.noreply.github.com>
2024-01-08 12:48:55 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
-rm -R Cpkg
+rm -Rf Cpkg
 mkdir Cpkg
 touch Cpkg/__init__.py
 cp ConV2erter/Updated_ConV2erter.py Cpkg/ConV2erter.py
-cp ConV2erter/Updatr.sh Cpkg.update.sh
+cp ConV2erter/Updatr.sh Cpkg/Cpkg.update.sh
 chmod +x Cpkg.update.sh
-rm -R ConV2erter/
+rm -Rf ConV2erter/
