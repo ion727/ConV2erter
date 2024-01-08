08b9eda3aca7acaeb38e93b2c681d0cf89e26a6f
ion727 <151961936+ion727@users.noreply.github.com>
2024-01-08 13:50:26 +0100

    Update Updatr.sh

--- Updatr.sh
+++ Updatr.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/ion727/ConV2erter.git && chmod +x ConV2erter/update.sh && ConV2erter/update.sh
+git clone https://github.com/ion727/ConV2erter.git && chmod +x ConV2erter/update.sh && ConV2erter/update.sh > /dev/null
